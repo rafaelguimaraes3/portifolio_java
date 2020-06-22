91502b93e0555da8cc2094ebb25f078927455dd5
rafaelguimaraes3 <rafael.guimaraes.tavares@gmail.com>
2020-06-22 00:42:27 -0300

    1º commit

---
+++ cursorafael/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+public class PrimeiroPrograma {
+    
+    public static void main (String args[]){
+
+    System.out.println("Meu primeiro programa");
+    System.out.println("Minha segunda linha de código");    
+    
+    }
+}
